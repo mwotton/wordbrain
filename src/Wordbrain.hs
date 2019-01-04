9e2bb4562e0eae5239676edad94592ba85c95ec8
Mark Wotton <mwotton@gmail.com>
2019-01-04 13:13:20 -0500

    initial commit of wordbrain

---
+++ src/Wordbrain.hs
@@ -0,0 +1,3 @@
+module Wordbrain where
+
+foo=1
---
+++ test/main.hs
@@ -0,0 +1,8 @@
+import Test.Hspec
+import Wordbrain
+
+main = hspec spec
+
+spec = describe "wordbrain" $ do
+  it "does stuff" $ do
+    1 `shouldBe` 1
