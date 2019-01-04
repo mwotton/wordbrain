d22186a569f4d599030ed9e04a25005180263050
Mark Wotton <mwotton@gmail.com>
2019-01-04 13:43:37 -0500

    readgrid

--- src/Wordbrain.hs
+++ src/Wordbrain.hs
@@ -1,3 +1,23 @@
 module Wordbrain where
+import Data.Map(partition, Map,fromList)
+import Data.Char(toLower, isAlpha)
+import Debug.Trace(trace)
 
-foo=1
+dotrace label s = trace (label <> ": " <> show s) s
+
+type Coord = (Int,Int)
+
+-- constraints:
+--   'a-z' plus ' ' only
+-- numbering system from top left corner
+readGrid :: String -> Either String (Map Coord Char)
+readGrid =
+  (\(ok,notOk) ->
+     if null notOk
+     then Right ok
+     else Left ("bad elements: " <> show notOk))
+  . partition isAlpha
+  . fromList
+  . filter ((/=' ') . snd)
+  . concatMap (\(rownum,row) -> zipWith (\col char -> ((rownum,col),char)) [0..] row)
+  . zip [0..] . lines . map toLower
--- test/main.hs
+++ test/main.hs
@@ -1,8 +1,15 @@
 import Test.Hspec
 import Wordbrain
+import qualified Data.Map as Map
+import Data.Either(isLeft)
 
 main = hspec spec
 
 spec = describe "wordbrain" $ do
-  it "does stuff" $ do
-    1 `shouldBe` 1
+  describe "readgrid" $ do
+    it "happy path" $ do
+      readGrid "abc\ndef\n" `shouldBe` Right (Map.fromList  [((0,0),'a'),((0,1),'b'),((0,2),'c'),((1,0),'d'),((1,1),'e'),((1,2),'f')])
+    it "rejects garbage" $ do
+      readGrid "123" `shouldSatisfy` isLeft
+    it "downcases" $ do
+      readGrid "abc\ndEf\n" `shouldBe` Right (Map.fromList  [((0,0),'a'),((0,1),'b'),((0,2),'c'),((1,0),'d'),((1,1),'e'),((1,2),'f')])
